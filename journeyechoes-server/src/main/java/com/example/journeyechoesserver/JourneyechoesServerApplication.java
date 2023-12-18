1c1e6b3ac088b5e43fc5b1f9dfe7179d2c524383
lai hung wei <128201642+s990093@users.noreply.github.com>
2023-12-19 02:19:44 +0800

    initial: server and web

--- journeyechoes-server/src/main/java/com/example/journeyechoesserver/JourneyechoesServerApplication.java
+++ journeyechoes-server/src/main/java/com/example/journeyechoesserver/JourneyechoesServerApplication.java
@@ -1,13 +1,13 @@
-package com.example.server;
+package com.example.journeyechoesserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerApplication {
+public class JourneyechoesServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+        SpringApplication.run(JourneyechoesServerApplication.class, args);
     }
 
 }
